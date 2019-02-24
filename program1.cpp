b8ca8c7f5069ee87d612e73660949f92455bf4f0
Michael Quinn <mquinn5@binghamton.edu>
2019-02-24 17:19:10 -0500

    removed some files

--- program1.cpp
+++ program1.cpp
@@ -1,7 +1,12 @@
 #ifndef DEBUG
 #define DEBUG true
 #endif
-#include "program1.h"
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <iostream>
+#include <string>
+#define BUFSIZE 256
 
 using namespace std;
 
@@ -10,38 +15,29 @@ void secret(void){
 	exit(0);
 }
 
-int stack_problem(char* arg){
-	cout << "In stack_problem: arg=" << arg << endl;
+void generate(char * str, unsigned int len){
+	for(unsigned int i=0; i<len; i++){
+		str[i]='s';
+	}
+}
+
+int main(int argc, char** argv){
+	char * arg;
+	if(argc>=3)
+		arg=argv[2];
+	else{
+		arg = (char *) malloc(sizeof(char) * (BUFSIZE+1));
+		generate(arg, BUFSIZE+1);
+	}
+	
+	cout << "In stack_problem: arg=" << argv[3] << endl;
+	
 	char buffer[BUFSIZE];
-	strcpy(buffer, arg);
+	strcpy(buffer, argv[3]);
 	
-	return 0;
-}
-
-int heap_problem(){
-	
-	return 0;
-}
-
-int prog_main(int argc, char** argv){
-#if DEBUG
-cout << "In program1:\n[";
-for(int i=0; i<argc; i++){
-	if(i>0)
-		cout << ", ";
-	cout << argv[i];
-}
-cout << "]" << endl;
-#endif
-	if(string(argv[1]).compare("stack")==0){
-		if(argc>=3)
-			return stack_problem(argv[2]);
-		else {
-			char emtstr = '\0';
-			return stack_problem(&emtstr);
-		}
-	} else if (string(argv[1]).compare("heap")==0){
-		return heap_problem();
+	if(argc<3){
+		free arg;
+		arg=NULL;
 	}
 	return 0;
 }
