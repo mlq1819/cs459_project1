d6d01a5fba38909475b54e66e177787e88f62ef2
Michael Quinn <mquinn5@binghamton.edu>
2019-02-24 13:49:46 -0500

    included some stuff

--- program1.h
+++ program1.h
@@ -1,5 +1,10 @@
 #ifndef MQ_PROGRAM1_H
 #define MQ_PROGRAM1_H
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <iostream>
+#include <string>
 
 void secret(void);
 int stack_problem(char*);
