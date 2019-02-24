8a831ff55a7059543751166a157b316b31249333
Michael Quinn <mquinn5@binghamton.edu>
2019-02-24 10:26:52 -0500

    moved functions

--- program1.cpp
+++ program1.cpp
@@ -14,18 +14,6 @@ void secret(void){
 	exit(0);
 }
 
-int main(int argc, char** argv){
-	if("stack".equals(argv[1])){
-		if(argc>=3)
-			return stack_problem(argv[2]);
-		else
-			return stack_problem("");
-	} else if ("heap".equals(argv[1])){
-		return heap_problem();
-	}
-	return 0;
-}
-
 int stack_problem(char* arg){
 	char buffer[BUFSIZE];
 	strcpy(buffer, argv[1]);
@@ -35,5 +23,17 @@ int stack_problem(char* arg){
 
 int heap_problem(){
 	
+	return 0;
+}
+
+int main(int argc, char** argv){
+	if("stack".equals(argv[1])){
+		if(argc>=3)
+			return stack_problem(argv[2]);
+		else
+			return stack_problem("");
+	} else if ("heap".equals(argv[1])){
+		return heap_problem();
+	}
 	return 0;
 }
